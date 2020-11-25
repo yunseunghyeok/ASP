9e2c46d2c915a842de287b2ffb5ce97795a0401e
yunseunghyeok <71643070+yunseunghyeok@users.noreply.github.com>
2020-11-25 15:58:41 +0900

    Create Home.asax.cs

---
+++ Home.asax.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _20203176YunSeungHyeok
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "현재접속인원 :" + Application["currentUser"] + " 명";
+            Label2.Text = "세션 연결 시간 : " + Session["begintime"];
+        }
+    }
+}
